249a7b5baeda165629af806428c8d7b8ffd3d07f
Jacob Schulz <jaschulz0705@gmail.com>
2021-07-25 18:45:00 -0700

    Updated Exercise_Library.js with compound exercises.
    Updated the list with compound exercises in place of isolation type exercises.

--- Exercise_Library.js
+++ Exercise_Library.js
@@ -1,5 +1,5 @@
 function GetValue () {
-		var myarray= new Array("Dive Bomber Push Up", "Goblet Squat", "Calf Raises", "Decline Push Up", "Chin Ups", "Decline Plank", "Squat Jump", "Downward Dog", "Hammer Curls", "Cobra Pose", "Chest Press", "Step Ups", "Pistol Squat", "Hanging Leg Raises", "Shoulder Push Ups", "Parallel Grip Pull Ups") ;
+		var myarray= new Array("Deadlift", "Overhead Press", "Front Squat", "Bench Press", "Back Squat", "Romanian Deadlift", "Clean and Jerk", "Push Press", "Sumo Deadlift", "Bulgarian Split Squat", "Pull Up", "Push Up", "Chin Up") ;
 		var random = myarray[Math.floor(Math.random() * myarray.length)];
 		document.getElementById("fill").innerHTML=random;
 		}
